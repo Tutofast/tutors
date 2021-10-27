c5c96ac3cec1060e869ed718c01ac0d19fb8a114
Alessandro <par_214@hotmail.com>
2021-10-26 22:49:32 -0500

    Initial commit an dev config

---
+++ src/main/java/com/arkisoftware/tutofasttutors/TutofastTutorsApplication.java
@@ -0,0 +1,13 @@
+package com.arkisoftware.tutofasttutors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutofastTutorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutofastTutorsApplication.class, args);
+    }
+
+}
